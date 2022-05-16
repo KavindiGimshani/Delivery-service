25d50bffa4a0dae57762ca4d550cfdaa24fd7b3e
KavindiGimshani <kavindigimshani999@gmail.com>
2022-05-17 00:59:13 +0530

    first commit

---
+++ models/Delivery.js
@@ -0,0 +1,70 @@
+
+const mongoose = require('mongoose');
+
+const DeliverySchema = new mongoose.Schema({
+
+    senderName: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    senderMobile: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    receiverName: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    receiverMobile: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    streetAddress: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    streetAddress2: { 
+        type: String,
+        required: false,
+        trim: true 
+    },
+
+    city: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    province: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    postalCode: { 
+        type: String,
+        required: true,
+        trim: true 
+    },
+
+    userId: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+});
+
+//export model to route
+const delivery = mongoose.model("Delivery", DeliverySchema);
+module.exports = delivery;
---
+++ routes/Delivery.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const DeliveryModel = require('../models/Delivery');
+
+//Add delivery details - USER TASK  
+//API : http://localhost:3001/delivery/deliveryDetails/${this.props.match.params.userId}
+router.route('/deliveryDetails/:userId').post(async (req, res) => {
+    if(req.body){
+        const Delivery = new DeliveryModel(req.body);
+        await Delivery.save()
+        .then(data => {
+            res.status(200).send({data: data});
+        }).catch(error => {
+            res.status(500).send({error: error});
+        })
+    }
+});
+
+//Update delivery details - USER TASK
+//API : http://localhost:3001/delivery/updateDeliveryDetail/${this.state.deliveryID}
+router.route("/updateDeliveryDetail/:id").put(async (req, res) => {
+    //Updating the offer details
+    const senderName = req.body.senderName;
+    const senderMobile = req.body.senderMobile;
+    const receiverName = req.body.receiverName;
+    const receiverMobile = req.body.receiverMobile;
+    const streetAddress = req.body.streetAddress;
+    const streetAddress2 = req.body.streetAddress2;
+    const province = req.body.province;
+    const city = req.body.city;
+    const postalCode = req.body.postalCode;
+    const userId = req.body.userId;
+    const amount = req.body.amount;
+
+    //Offer ID
+    const Id = req.params.id;
+
+    try {
+        await DeliveryModel.findById(Id, (err, updatedDeliveryObject) => {
+            updatedDeliveryObject.senderName = senderName;
+            updatedDeliveryObject.senderMobile = senderMobile;
+            updatedDeliveryObject.receiverName = receiverName;
+            updatedDeliveryObject.receiverMobile = receiverMobile;
+            updatedDeliveryObject.streetAddress = streetAddress;
+            updatedDeliveryObject.streetAddress2 = streetAddress2;
+            updatedDeliveryObject.province = province;
+            updatedDeliveryObject.city = city;
+            updatedDeliveryObject.postalCode = postalCode;
+            updatedDeliveryObject.userId = userId;
+            updatedDeliveryObject.amount = amount;
+
+            updatedDeliveryObject.save()
+                .then(data => {
+                    res.status(200).send({ data: data });
+                }).catch(error => {
+                    res.status(500).send({ error: error });
+                })
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+//Get delivery by userId for update - USER TASK
+//API : http://localhost:3001/delivery/deliveryForCustomer/${this.props.match.params.userId}
+router.route("/deliveryForCustomer/:userId").get(async (req, res) => {
+    const currentuserId = req.params.userId;
+
+    DeliveryModel.find({ userId: currentuserId})
+        .then(data => {
+            res.status(200).send({ data: data });
+        }).catch(error => {
+            res.status(500).send({ error: error });
+        })
+}); 
+
+//View delivery by userId - USER TASK
+//API : http://localhost:3001/delivery/viewDeliveryDetails/${this.props.match.params.userId}/${this.props.match.params.amount}
+router.route("/viewDeliveryDetails/:userId/:amount").get(async (req, res) => {
+    const currentuserId = req.params.userId;
+
+    DeliveryModel.find({ userId: currentuserId})
+        .then(data => {
+            res.status(200).send({ data: data });
+        }).catch(error => {
+            res.status(500).send({ error: error });
+        })
+}); 
+
+module.exports = router;
